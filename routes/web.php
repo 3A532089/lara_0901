d0585aa4ffdef122d1212856fce7b15e2f181e50
3A532089 <likebestupid@gmail.com>
2018-12-31 11:06:31 +0800

    設定路由

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,9 @@
 Route::get('/', function () {
     return view('welcome');
 });
+Route::group(['prefix' => 'admin'], function() {
+    Route::get('/', ['as' => 'admin.dashboard.index', 'uses' =>'AdminDashboardController@index']);
+    Route::get('posts' , ['as' => 'admin.posts.index' , 'uses'=> 'AdminPostsController@index']);
+    Route::get('posts/create' , ['as' => 'admin.posts.create', 'uses'=> 'AdminPostsController@create']);
+    Route::get('posts/{id}/edit', ['as' => 'admin.posts.edit' , 'uses'=> 'AdminPostsController@show']);
+});
