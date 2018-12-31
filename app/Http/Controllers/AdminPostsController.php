fdd570c893700c44fcb34326b84e7f40da712adb
3A532089 <likebestupid@gmail.com>
2018-12-31 16:11:18 +0800

    設定Form Request

--- app/Http/Controllers/AdminPostsController.php
+++ app/Http/Controllers/AdminPostsController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-use App\Http\Requests;
+use App\Http\Requests\PostRequest;
 
 use App\Post;
 
@@ -17,7 +17,7 @@ class AdminPostsController extends Controller
         return view('admin.posts.index','$data');
     }
 
-    public function store(Request $request)
+    public function store(PostRequest $request)
     {
         Post::create($request->all());
         return redirect()->route('admin.posts.index');
@@ -36,7 +36,7 @@ class AdminPostsController extends Controller
         return view('admin.posts.edit', $data);
     }
 
-    public function update(Request $request, $id)
+    public function update(PostRequest $request, $id)
     {
         $post=Post::find($id);
         $post->update($request->all());
